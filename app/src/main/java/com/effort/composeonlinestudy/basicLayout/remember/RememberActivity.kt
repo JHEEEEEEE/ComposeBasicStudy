f59dd0be8a01438b3615816d6aebfe5911e00871
Jeonghyeon <jeonghyeon0208@g.hongik.ac.kr>
2025-02-24 20:49:59 +0900

    make counter using Remember

--- app/src/main/java/com/effort/composeonlinestudy/basicLayout/remember/RememberActivity.kt
+++ app/src/main/java/com/effort/composeonlinestudy/basicLayout/remember/RememberActivity.kt
@@ -4,24 +4,52 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.effort.composeonlinestudy.ui.theme.ComposeOnlineStudyTheme
 
-class RememberActivity: ComponentActivity() {
+class RememberActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeOnlineStudyTheme {
-                Surface (
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-
-                }
+                SimpleCounterBtn()
             }
         }
+    }
+}
+
+
+@Composable
+fun SimpleCounterBtn() {
+    var count by remember { mutableStateOf(0) }
+
+    Button(
+        onClick = {
+            count++
+        },
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(
+            text = "Count : $count",
+            fontSize = 50.sp
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewSimpleCounterBtn() {
+    ComposeOnlineStudyTheme {
+        SimpleCounterBtn()
     }
 }
