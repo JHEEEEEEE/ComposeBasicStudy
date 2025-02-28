c93067a843171b0007a186ca6e2746c6eb43e71c
Jeonghyeon <jeonghyeon0208@g.hongik.ac.kr>
2025-02-28 14:14:07 +0900

    Implement Three Graphs with navigation

---
+++ app/src/main/java/com/effort/composeonlinestudy/practice/graph/NavigationItem.kt
@@ -0,0 +1,9 @@
+package com.effort.composeonlinestudy.practice.graph
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class NavigationItem(
+    val name: String,
+    val icon: ImageVector,
+    val route: String
+)
